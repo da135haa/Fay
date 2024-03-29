3f4f04a8535984959eca04b12c3fb754a8e5f928
Steven <da135haa@gmail.com>
2024-03-29 15:49:00 +0800

    調整資料刪除邏輯

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def __clear_samples():
     if not os.path.exists("./samples"):
         os.mkdir("./samples")
     for file_name in os.listdir('./samples'):
-        if file_name.startswith('sample-') and file_name.endswith('.mp3'):
+        # if file_name.startswith('sample-') and file_name.endswith('.mp3'):
+        if file_name.startswith('sample-'):
             os.remove('./samples/' + file_name)
-
 
 def __clear_songs():
     if not os.path.exists("./songs"):
